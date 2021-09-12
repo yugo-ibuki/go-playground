9cdbf6b64439789dd559b9724e8e09afd805de5d
yugo-ibuki <yugo.ibuki@asia-quest.jp>
2021-09-12 22:44:30 +0900

    インターフェースを作る

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "net/http"
+import "fmt"
 
 func main() {
-	engine:= gin.Default()
-	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
-	engine.Run(":3001")
+	var obj interface{}
+
+	obj = 0123                                                            // int
+	obj = "String"                                                       // string
+	obj = []string{"Python", "Golang", "Ruby"}                          // slice
+	//obj = func greetings(_ string) string { return "Hello World" }     // function
+
+	fmt.Println(obj)
 }
+
+// Ginを使用したhttpサーバ
+//func main() {
+//	engine:= gin.Default()
+//	engine.GET("/", func(c *gin.Context) {
+//		c.JSON(http.StatusOK, gin.H{
+//			"message": "hello world",
+//		})
+//	})
+//	engine.Run(":3001")
+//}
 
 //import "fmt"
 //
